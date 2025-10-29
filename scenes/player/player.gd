33b58d18ee6ec975e0df04523b74b6fc79501403
lxviolet <lxviolet@proton.me>
2025-10-29 03:34:33 +0000

    Recoded movement to be better (no i will NOT elaborate)

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -28,28 +28,52 @@ enum Movestates {
 @export_category("Curves")
 @export var strafe_curve: Curve
 
-# NOTICE: maybe for snap lengths, define them programmatically as the
-# resulting velocity from moving in the current state
+# NOTICE: maybe for snap speeds, define them programmatically as the
+# resulting velocity from moving in the current state, so it never stops you
+# from moving and might also feel more intuitive and better, but idk this
+# sounds both complicated and also really computationally expensive (having to
+# calculate the entire movement physics process just to figure out the snap
+# speed). also
+
+@export var ground_speed_curve: Curve
+@export var ground_accel_curve: Curve
+@export var ground_drag_curve: Curve
+@export var ground_snap_speed_curve: Curve
+
+@export var air_speed_curve: Curve
+@export var air_accel_curve: Curve
+@export var air_drag_curve: Curve
+@export var air_snap_speed_curve: Curve
+
+@export var slide_speed_curve: Curve
+@export var slide_accel_curve: Curve
+@export var slide_drag_curve: Curve
+@export var slide_snap_speed_curve: Curve
+
+@export var wallrun_speed_curve: Curve
+@export var wallrun_accel_curve: Curve
+@export var wallrun_drag_curve: Curve
+@export var wallrun_snap_speed_curve: Curve
 
 const GROUND_SPEED: float = 50.0
 const GROUND_ACCEL: float = 2.0
-const GROUND_DRAG: float = 13.0
-const GROUND_SNAP_LENGTH: float = 0.2
+const GROUND_DRAG: float = 5.0
+const GROUND_SNAP_SPEED: float = 0.2
 
 const AIR_SPEED: float = 9.0
 const AIR_ACCEL: float = 1.0
 const AIR_DRAG: float = 0.0
-const AIR_SNAP_LENGTH: float = 0.05
+const AIR_SNAP_SPEED: float = 0.05
 
-const SLIDE_SPEED: float = 15.0
-const SLIDE_ACCEL: float = 0.3
+const SLIDE_SPEED: float = GROUND_SPEED * 2
+const SLIDE_ACCEL: float = GROUND_ACCEL / 2
 const SLIDE_DRAG: float = 2.0
-const SLIDE_SNAP_LENGTH: float = GROUND_SNAP_LENGTH
+const SLIDE_SNAP_SPEED: float = GROUND_SNAP_SPEED
 
 const WALLRUN_SPEED: float = 20.0
 const WALLRUN_ACCEL: float = 0.0
-const WALLRUN_DRAG: float = 0.0
-const WALLRUN_SNAP_LENGTH: float = AIR_SNAP_LENGTH
+const WALLRUN_DRAG: float = 0.0 # maybe this should just be `AIR_DRAG`?
+const WALLRUN_SNAP_SPEED: float = AIR_SNAP_SPEED
 
 const JUMP_POWER: float = 1.5
 const JUMP_BOOST_DECREMENT: float = JUMP_POWER / 5
@@ -61,18 +85,19 @@ const MAX_COYOTE_TIME: float = 100
 ## "MOUSE_SENSITIVITY" SHALL REMAIN AT A REASONABLE QUANTITY
 const DEGREES_PER_UNIT: float = 0.01
 
-# this is not a "speed cap", just when to stop applying gravity.
 # there will perhaps maybe in future be something that lets you instantly set
 # your Y velocity to this (basically stolen from parkour legacy downshift)
 # ok maybe not EXACTLY this value but a similarly high value
-const TERMINAL_VELOCITY: float = 100.0
+const MAX_FALL_SPEED: float = 100.0
 
 ## ZIS VARIABLE CONTAINS DAS VECTOR VHICH ZE PLAYER VISHES TO TRAVEL IN
 ## FOR INSTANCE, IF ZE PLAYER HOLDS FORWARD ZEN ZE VARIABLE SHALL HOLD ZE
 ## VALUE (0, -1)
 ## [br][br]
 ## IT IS OF MOST IMPORTANCE YOU KEEP IN MIND ZAT ZE VECTOR IS OF LENGTH 1
-var wish_dir := Vector2.ZERO
+var wish_dir := Vector3.ZERO
+## ZIS VARIABLE IS MISSING DOCUMENTATION
+var wish_vel: Vector3
 ## ZIS VARIABLE CONTAINS EIN BOOLEAN VALUE VHICH TELLS US IF ZE PLAYER VISHES
 ## TO JUMP
 var wish_jump: bool = false
@@ -108,11 +133,12 @@ var jump_boost: float = 0
 var jumping: bool = false
 
 func _ready() -> void:
-	# 20 bucks says this line of code will break within a year (17/09/2025)
-	Input.set_use_accumulated_input(false)
-	
 	for node in get_tree().get_nodes_in_group("Player PhysicsBody3Ds"):
 		our_physics_body_ids.append(node.get_instance_id())
+		
+	if is_multiplayer_authority():
+		# 20 bucks says this line of code will break within a year (17/09/2025)
+		Input.set_use_accumulated_input(false)
 
 
 func _physics_process(_delta) -> void:
@@ -120,6 +146,9 @@ func _physics_process(_delta) -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not is_multiplayer_authority():
+		return
+		
 	if event is InputEventKey:
 		handle_keyboard_input(event)
 	
@@ -131,75 +160,75 @@ func _unhandled_input(event: InputEvent) -> void:
 
 
 func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
+	var horizontal_velocity: Vector3 = linear_velocity * Vector3(1, 0, 1)
+	
 	var things_touching_legs: Array = GroundArea.get_overlapping_bodies()
 	movestate = determine_movestate(things_touching_legs)
 
 	# Godot team please just add array/tuple unpacking to core already
 	# this is just getting embarassing for you guys
-	var __: Dictionary[String,float] = get_movestate_stats(movestate)
+	var __: Dictionary[String,float] = get_movement_stats(
+			movestate, horizontal_velocity.length())
 	var move_speed: float = __["speed"]
 	var move_accel: float = __["accel"]
-	var move_drag: float = __["drag"]
-	var snap_length: float = __["snap_length"]
-	
-	var wish_vel: Vector2 = wish_dir.rotated(-Camera.rotation.y) * move_speed
-	var horizontal_velocity = Vector2(linear_velocity.x, linear_velocity.z)
-	#var wish_vel: Vector2 = (
-			#wish_dir * horizontal_velocity \
-			#if horizontal_velocity.length() > move_speed
-			#else wish_dir * move_speed
-	#)
+	var move_drag:  float = __["drag"]
+	var snap_speed: float = __["snap_speed"]
 	
 	## Angle between [member linear_velocity] and [member wish_vel]
-	var strafe_angle: float = abs(rad_to_deg(wish_vel.angle_to(
-			horizontal_velocity)))
-	
+	var strafe_angle: float = deg_to_rad(wish_vel.angle_to(
+			linear_velocity * Vector3(1, 0, 1)))
 	var strafe_multiplier: float = strafe_curve.sample(strafe_angle)
 	print("%.3f %.3f" % [strafe_angle, strafe_multiplier])
 	
-	wish_vel *= strafe_multiplier
+	## [member linear_velocity] with the [code]y[/code] component set to 0,
+	## just exists because doing
+	## [code]linear_velocity * Vector3(1, 0, 1)[/code] every time we want the
+	## velocity with the vertical component removed (quite often) is less
+	
+	#wish_vel *= strafe_multiplier
 	
 	if movestate == Movestates.GROUND:
 		coyote_time = MAX_COYOTE_TIME
 	
-	if wish_jump:
-		if coyote_time > 0:
-			# Jump
-			jump_boost = JUMP_POWER
-			coyote_time = 0
-		# Maintain jump
-		if jump_boost > 0:
-			linear_velocity.y += jump_boost
-	else:
-		jump_boost = 0
-	jump_boost -= JUMP_BOOST_DECREMENT
-	# FIXME: doing it like this is MID!!! the drag should be applied no matter
-	# what, but rn it applies drag if wish_vel is (0, 0) and otherwise
-	# it applies acceleration it should do both!!! wtf was i cooking!!!
+	elif movestate == Movestates.AIR:
+		pass
+	
+	elif movestate == Movestates.WALLRUN:
+		pass
+	
+	elif movestate == Movestates.SLIDE:
+		pass
+	
+	handle_jump()
+	
+	wish_vel = wish_dir.rotated(Vector3.UP, Camera.rotation.y) * move_speed
+	
+	# If wish_vel < linear_velocity, then we set the length to that of
+	# linear velocity
+	if wish_vel.length() < horizontal_velocity.length():
+		wish_vel = Utils.set_length(wish_vel, horizontal_velocity.length())
+	
+	# Apply player movement
+	if wish_vel != Vector3.ZERO:
+		linear_velocity = Utils.exp_decay(
+				linear_velocity,
+				wish_vel,
+				move_accel,
+				state.step,
+		)
+	# Apply drag
 	linear_velocity = Utils.exp_decay(
 			linear_velocity,
-			Vector3(wish_vel.x, linear_velocity.y, wish_vel.y),
-			move_accel,
-			state.step, # functionally identical to `delta`
-	)
-	linear_velocity = Utils.exp_decay(
-			linear_velocity,
-			Vector3(0, 0, 0),
+			linear_velocity * Vector3(0, 1, 0),
 			move_drag,
-			state.step, # functionally identical to `delta`
+			state.step,
 	)
 	
-	# maybe this should only be applied if the player isn't on the ground;
-	# having the player rconstantly being pushed into the ground might not
-	# be the best (but also at the same time kinda is good for physics e.g
-	# you would probably want to be pushing to the platform you're standing on
-	# if that platform is a physics object)
-	if linear_velocity.y < TERMINAL_VELOCITY:
+	if (linear_velocity * abs(gravity_vector)).length() < MAX_FALL_SPEED:
 		linear_velocity += gravity_vector * state.step
 	
-	if (linear_velocity * Vector3(1, 0, 1)).length() < snap_length:
+	if (linear_velocity * Vector3(1, 0, 1)).length() < snap_speed:
 		linear_velocity *= Vector3(0, 1, 0)
-	
 	
 	update_debug_information(
 			wish_jump,
@@ -211,6 +240,20 @@ func _integrate_forces(state: PhysicsDirectBodyState3D) -> void:
 			coyote_time,
 			movestate,
 	)
+
+
+func handle_jump() -> void:
+	if not wish_jump:
+		jump_boost = 0
+		return
+	if coyote_time > 0:
+		# Jump
+		jump_boost = JUMP_POWER
+		coyote_time = 0
+		# Maintain jump
+	if jump_boost > 0:
+		linear_velocity.y += jump_boost
+	jump_boost -= JUMP_BOOST_DECREMENT
 
 
 ## We Calleth Uponeth this Functionalitye at everye just opportunity with which
@@ -228,10 +271,11 @@ func handle_keyboard_input(event: InputEventKey) -> void:
 		elif Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
 			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	
-	wish_dir = Input.get_vector(
+	var input_vector: Vector2 = Input.get_vector(
 			"move_left", "move_right",
 			"move_forward", "move_backward"
 	)
+	wish_dir = Vector3(input_vector.x, 0, input_vector.y)
 	
 	# "Don't Repeat Yourself" is advice for cowards
 	if event.is_action_pressed("jump"):
@@ -250,7 +294,7 @@ func handle_keyboard_input(event: InputEventKey) -> void:
 func handle_mouse_movement(event: InputEventMouseMotion) -> void:
 	## ZIS VARIABLE CONTAINS DAS UNSCALED POINTING DEVICE MOTION RELATIVE TO
 	## DAS PREVIOUS POSITION IN ZE COORDINATE SYSTEM OF ZE SCREEN
-	var motion = event.screen_relative
+	var motion: Vector2 = event.screen_relative
 	rotation_difference += motion * mouse_sensitivity * DEGREES_PER_UNIT
 
 
@@ -271,7 +315,7 @@ func fetch_rotation_difference() -> Vector2:
 	
 
 func determine_movestate(things_touching_legs: Array[Node3D]) -> Movestates:
-	for i in things_touching_legs:
+	for i: Node3D in things_touching_legs:
 		if i.get_instance_id() in our_physics_body_ids:
 			things_touching_legs.erase(i)
 	
@@ -290,15 +334,16 @@ func determine_movestate(things_touching_legs: Array[Node3D]) -> Movestates:
 		# be implemented.
 		return Movestates.AIR
 
+
 func update_debug_information(
 	given_wish_jump: bool,
 	given_wish_slide: bool,
-	given_wish_dir: Vector2,
-	given_wish_vel: Vector2,
+	given_wish_dir: Vector3,
+	given_wish_vel: Vector3,
 	given_position: Vector3,
 	given_velocity: Vector3,
 	given_coyote_time: float,
-	given_state: Movestates,
+	given_movestate: Movestates,
 ) -> void:
 	WishJumpLabel.text = "%s" % given_wish_jump
 	WishSlideLabel.text = "%s" % given_wish_slide
@@ -308,46 +353,78 @@ func update_debug_information(
 			% [given_position.x, given_position.y, given_position.z]
 	HorizontalVelocityLabel.text = "%.3f" \
 			% Vector2(given_velocity.x, given_velocity.z).length()
-	VerticalVelocityLabel.text = "%.3f" % given_velocity.y
+	VerticalVelocityLabel.text = "%.3f" % abs(given_velocity.y)
 	CoyoteTimeLabel.text = "%.3f" % given_coyote_time
-	MovestateLabel.text = "%s" % given_state
+	MovestateLabel.text = movestate_to_string(given_movestate)
 
 
-func get_movestate_stats(given_movestate) -> Dictionary[String, float]:
+func movestate_to_string(given_movestate) -> String:
+	return (
+			"Ground" if given_movestate == Movestates.GROUND
+			else "Air" if given_movestate == Movestates.AIR
+			else "Slide" if given_movestate == Movestates.SLIDE
+			else "Wallrun"
+	)
+
+
+func get_movement_stats(
+		given_movestate: Movestates,
+		given_velocity: float,
+) -> Dictionary[String,float]:
 	match given_movestate:
 		Movestates.GROUND:
 			return {
-				"speed": GROUND_SPEED,
-				"accel": GROUND_ACCEL,
-				"drag": GROUND_DRAG,
-				"snap_length": GROUND_SNAP_LENGTH,
+				"speed": ground_speed_curve.sample(given_velocity) \
+				* GROUND_SPEED,
+				"accel": ground_accel_curve.sample(given_velocity) \
+				* GROUND_ACCEL,
+				"drag": ground_drag_curve.sample(given_velocity) \
+				* GROUND_DRAG,
+				"snap_speed": ground_snap_speed_curve.sample(given_velocity) \
+				* GROUND_SNAP_SPEED,
 			}
 		Movestates.AIR:
 			return {
-				"speed": AIR_SPEED,
-				"accel": AIR_ACCEL,
-				"drag": AIR_DRAG,
-				"snap_length": AIR_SNAP_LENGTH,
+				"speed": air_speed_curve.sample(given_velocity) \
+				* AIR_SPEED,
+				"accel": air_accel_curve.sample(given_velocity) \
+				* AIR_ACCEL,
+				"drag": air_drag_curve.sample(given_velocity) \
+				* AIR_DRAG,
+				"snap_speed": air_snap_speed_curve.sample(given_velocity) \
+				* AIR_SNAP_SPEED,
 			}
 		Movestates.SLIDE:
 			return {
-				"speed": SLIDE_SPEED,
-				"accel": SLIDE_ACCEL,
-				"drag": SLIDE_DRAG,
-				"snap_length": SLIDE_SNAP_LENGTH,
+				"speed": slide_speed_curve.sample(given_velocity) \
+				* SLIDE_SPEED,
+				"accel": slide_accel_curve.sample(given_velocity) \
+				* SLIDE_ACCEL,
+				"drag": slide_drag_curve.sample(given_velocity) \
+				* SLIDE_DRAG,
+				"snap_speed": slide_snap_speed_curve.sample(given_velocity) \
+				* SLIDE_SPEED,
 			}
 		Movestates.WALLRUN:
 			return {
-				"speed": WALLRUN_SPEED,
-				"accel": WALLRUN_ACCEL,
-				"drag": WALLRUN_DRAG,
-				"snap_length": WALLRUN_SNAP_LENGTH,
+				"speed": wallrun_speed_curve.sample(given_velocity) \
+				* WALLRUN_SPEED,
+				"accel": wallrun_accel_curve.sample(given_velocity) \
+				* WALLRUN_ACCEL,
+				"drag": wallrun_drag_curve.sample(given_velocity) \
+				* WALLRUN_DRAG,
+				"snap_speed": wallrun_snap_speed_curve.sample(given_velocity) \
+				* WALLRUN_SPEED,
 			}
 	
-	# this should never happen, and exists just to shut up a godot warning
-	# how about if it somehow does, somthing funny happens?
+	# godot spits out an error because "not all code paths return a value"
+	# (we always have a movestate, so we always return something, but godot
+	# disagrees :3)
+	# if somehow you break the code this spectacularly, how about you get a
+	# spectacle in return
 	return {
-		"speed": 24727,
-		"accel": 24727,
-		"drag": 0,
+		"speed":      24727,
+		"accel":      24727,
+		"drag":       0,
+		"snap_speed": 0,
 	}
